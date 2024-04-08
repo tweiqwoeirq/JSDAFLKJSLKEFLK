d85b9113a920c29710289c02c84dab994b67d1b7
Artem (Prikhodko) <161821453+tweiqwoeirq@users.noreply.github.com>
2024-04-08 11:36:48 +0000

    1

--- 3/Test.java
+++ 3/Test.java
@@ -1,25 +1,41 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Test {
     public static void main(String[] args) {
-        double Mass = 10.5;
-        double Height = 20.0;
+        double mass = 10.5;
+        double height = 20.0;
 
-        double MaxEnergy = EnergyCalculator.calculateMaxEnergy(Mass, Height);
+        double maxEnergy = EnergyCalculator.calculateMaxEnergy(mass, height);
 
-        CalculationData data = new CalculationData(Mass, Height, MaxEnergy);
+        CalculationData data = new CalculationData(mass, height, maxEnergy);
 
         try {
-            System.out.println("Serialized data: \n" + data + "\n");
-
-            SerializationDemo.serializeObject(data, "data.ser");
-            CalculationData deserializedData = (CalculationData) SerializationDemo.deserializeObject("data.ser");
-
-            System.out.println("Deserialized data: \n" + deserializedData + "\n");
-
-            // Використання фабричного методу для відображення результатів
-            Displayable displayable = DisplayFactory.createDisplayable(deserializedData);
-            displayable.display();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String input;
+            do {
+                System.out.println("Enter a command ('view', 'quit', 'save'):");
+                input = reader.readLine().trim();
+                switch (input) {
+                    case "view":
+                        System.out.println("Serialized data: \n" + data + "\n");
+                        CalculationData deserializedData = (CalculationData) SerializationDemo.deserializeObject("data.ser");
+                        System.out.println("Deserialized data: \n" + deserializedData + "\n");
+                        Displayable displayable = DisplayFactory.createDisplayable(data);
+                        displayable.display();
+                        break;
+                    case "quit":
+                        System.out.println("Exiting program...");
+                        break;
+                    case "save":
+                        SerializationDemo.serializeObject(data, "data.ser");
+                        System.out.println("Data saved successfully.");
+                        break;
+                    default:
+                        System.out.println("Invalid command. Please try again.");
+                }
+            } while (!input.equals("quit"));
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
