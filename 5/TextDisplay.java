39707be7ae32a92f446dd7d47f5d081a484615da
Artem (Prikhodko) <161821453+tweiqwoeirq@users.noreply.github.com>
2024-04-16 22:18:34 +0300

    Update TextDisplay.java

--- 5/TextDisplay.java
+++ 5/TextDisplay.java
@@ -1,18 +1,21 @@
+// Клас, що реалізує інтерфейс Displayable для відображення даних у текстовому форматі
 public class TextDisplay implements Displayable {
-    private CalculationData data;
+    private CalculationData data; // Поля для зберігання даних про обчислення
 
+    // Конструктор класу для ініціалізації даних
     public TextDisplay(CalculationData data) {
         this.data = data;
     }
 
+    // Метод для відображення даних у вигляді таблиці
     @Override
     public void display() {
         System.out.println("--------------------------------------------------");
         System.out.println("| Параметр           | Значення               |");
         System.out.println("--------------------------------------------------");
-        System.out.println(String.format("| %-20s | %-20.2f |", "Маса", data.getMass()));
-        System.out.println(String.format("| %-20s | %-20.2f |", "Висота", data.getHeight()));
-        System.out.println(String.format("| %-20s | %-20.2f |", "Максимальна енергія", data.getMaxEnergy()));
+        System.out.println(String.format("| %-20s | %-20.2f |", "Маса", data.getMass())); // Виведення маси
+        System.out.println(String.format("| %-20s | %-20.2f |", "Висота", data.getHeight())); // Виведення висоти
+        System.out.println(String.format("| %-20s | %-20.2f |", "Максимальна енергія", data.getMaxEnergy())); // Виведення максимальної енергії
         System.out.println("--------------------------------------------------");
     }
 }
