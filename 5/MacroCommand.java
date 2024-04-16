445976a5e1e243a665810f77cab41c457d8beabd
Artem (Prikhodko) <161821453+tweiqwoeirq@users.noreply.github.com>
2024-04-16 22:19:28 +0300

    Update MacroCommand.java

--- 5/MacroCommand.java
+++ 5/MacroCommand.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// Клас, що представляє макрокоманду, яка може виконувати групу команд одночасно
 public class MacroCommand implements Command {
-    private List<Command> commands;
+    private List<Command> commands; // Список команд, що входять до макрокоманди
 
     public MacroCommand() {
         commands = new ArrayList<>();
     }
 
+    // Метод для додавання команди до макрокоманди
     public void addCommand(Command command) {
         commands.add(command);
     }
@@ -15,14 +17,14 @@ public class MacroCommand implements Command {
     @Override
     public void execute() {
         for (Command command : commands) {
-            command.execute();
+            command.execute(); // Виконати кожну команду в макрокоманді
         }
     }
 
     @Override
     public void undo() {
         for (int i = commands.size() - 1; i >= 0; i--) {
-            commands.get(i).undo();
+            commands.get(i).undo(); // Відмінити кожну команду в макрокоманді у зворотньому порядку
         }
     }
 }
