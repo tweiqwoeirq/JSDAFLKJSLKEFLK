61ee44e09c0d0476fefd39168ebabbd147a624ad
Artem (Prikhodko) <161821453+tweiqwoeirq@users.noreply.github.com>
2024-04-16 22:20:32 +0300

    Update CommandManager.java

--- 5/CommandManager.java
+++ 5/CommandManager.java
@@ -1,13 +1,15 @@
 import java.util.Stack;
 
+// Клас, що керує командами, використовуючи стек для зберігання історії виконаних команд
 public class CommandManager {
     private static CommandManager instance;
-    private Stack<Command> history;
+    private Stack<Command> history; // Стек для зберігання виконаних команд
 
     private CommandManager() {
         history = new Stack<>();
     }
 
+    // Метод для отримання єдиного екземпляру CommandManager (шаблон Singleton)
     public static CommandManager getInstance() {
         if (instance == null) {
             instance = new CommandManager();
@@ -15,11 +17,13 @@ public class CommandManager {
         return instance;
     }
 
+    // Метод для виконання команди та додавання її до історії
     public void executeCommand(Command command) {
         command.execute();
         history.push(command);
     }
 
+    // Метод для відміни останньої виконаної команди
     public void undoLastCommand() {
         if (!history.isEmpty()) {
             Command lastCommand = history.pop();
