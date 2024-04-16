675e4db88c26a368d12d1a00b2d3da36a5a803b3
Artem (Prikhodko) <161821453+tweiqwoeirq@users.noreply.github.com>
2024-04-16 22:20:14 +0300

    Update DisplayFactory.java

--- 5/DisplayFactory.java
+++ 5/DisplayFactory.java
@@ -1,4 +1,6 @@
+// Фабричний клас для створення об'єктів типу Displayable
 public class DisplayFactory {
+    // Метод для створення об'єкта типу Displayable на основі CalculationData
     public static Displayable createDisplayable(CalculationData data) {
         return new TextDisplay(data);
     }
